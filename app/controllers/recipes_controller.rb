01f3c8973ecdf0a54c0667ed10829b2afef3ffb8
hajjboy95 <gizmo-boy95@hotmail.com>
2015-08-04 20:16:34 -0700

    added css , logic to handle image upload , form , and nice index page

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,67 @@
+class RecipesController < ApplicationController
+
+  before_action :find_recipe , only:[:show,:edit,:update,:destroy]
+
+
+  def index
+    @recipe = Recipe.all.order("created_at DESC")
+  end
+
+
+  def show
+
+
+  end
+
+  def new
+    @recipe = Recipe.new
+
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      redirect_to @recipe , notice: "Suceessfully created new recipe"
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+
+
+  end
+
+  def update
+
+    if @recipe.update(recipe_params)
+
+      redirect_to @recipe , notice:"Sucessfully created new Recipe"
+
+    else
+      render 'edit'
+    end
+
+
+
+  end
+
+  def destroy
+    @recipe.destroy!
+    redirect_to root_path , notice: "Successfully deltered recipe"
+
+
+
+  end
+
+  private
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title,:description,:image)
+  end
+end
